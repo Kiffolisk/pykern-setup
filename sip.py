17cd52358542e3fd845f818afbd4ea640066707c
NitrogenDioxide <45130240+Kiffolisk@users.noreply.github.com>
2021-11-14 21:54:10 +0100

    sip

---
+++ sip.py
@@ -0,0 +1,15 @@
+import os
+import urllib.request
+
+def run(osdir, username, curpath, args):
+    print("Attempting to install package " + args[1])
+    canDownload = True
+    if canDownload == True:
+        try:
+            url = "https://raw.githubusercontent.com/Kiffolisk/pykern-sip/main/" + args[1] + ".py"
+            print("Downloading to " + curpath + "/" + args[1] + ".py")
+            urllib.request.urlretrieve(url, osdir + "/user/" + username + "/pkg/" + args[1] + ".py")
+        except:
+            print("Failed to install package!")
+    else:
+        print("Please move to /user/YOURUSERNAME/pkg.")
